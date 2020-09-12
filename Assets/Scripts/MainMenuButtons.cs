455886004e2d161fe0d560dd5f70121098fd7498
Natalie Bloniarz <bloniarzna@gmail.com>
2020-09-11 20:47:41 -0400

    Added Simple Main Menu
    Added a simple main menu and set up async loading for when scenes get mroe complex and have longer loading times

---
+++ Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour {
+
+    private bool startLoad = false;
+    private string mainGameScene = "";
+    public GameObject loadingIndicator;
+    public GameObject settingsUI;
+
+    public void startLoadGame(string sceneName) {
+        mainGameScene = sceneName;
+        startLoad = true;
+        loadingIndicator.SetActive(true);
+    }
+
+    private void Update() {
+        if (startLoad) {
+            StartCoroutine(loadMainGame());
+            startLoad = false;
+        }
+    }
+
+    private IEnumerator loadMainGame() {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(mainGameScene);
+
+        while (!asyncLoad.isDone) {
+            yield return null;
+        }
+    }
+
+    public void showSettings() {
+        settingsUI.SetActive(true);
+    }
+
+    public void hideSettings() {
+        settingsUI.SetActive(false);
+    }
+}
